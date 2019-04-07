79432aa976b285416a5348f09cad5fe1875a86f6
Vitalii Havryliuk <v.havryliuk@noisyminer.com>
2019-04-07 10:27:35 +0300

    Initial Commit

---
+++ Scanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scanner
+//
+//  Created by Vitalii Havryliuk on 4/7/19.
+//  Copyright © 2019 Vitalii Havryliuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
